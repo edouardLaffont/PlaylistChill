958c1179da1c20eff2867888373cf8dedc181400
edouardLaffont <edouard.laffont@live.com>
2022-03-08 09:25:20 +0100

    init spotify API - WIP scope

---
+++ apps/back/app/controllers/spotifyController.js
@@ -0,0 +1,87 @@
+const spotifyController = (function() {
+    
+    const clientId = 'b319a600fd7d4cdd968a4f1bf7b9b585';
+    const clientSecret = '87c3c36d09c546cbb643b440c6a56c25';
+
+   
+    const getToken = async () => {
+
+        const result = await fetch('https://accounts.spotify.com/api/token', {
+            method: 'POST',
+            headers: {
+                'Content-Type' : 'application/x-www-form-urlencoded', 
+                'Authorization' : 'Basic ' + btoa(clientId + ':' + clientSecret)
+            },
+            body: 'grant_type=client_credentials'
+        });
+
+        const data = await result.json();
+        return data.access_token;
+    }
+    
+    const getGenres = async (token) => {
+
+        const result = await fetch(`https://api.spotify.com/v1/browse/categories?locale=sv_US`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data.categories.items;
+    }
+
+    const getPlaylistByGenre = async (token, genreId) => {
+
+        const limit = 10;
+        
+        const result = await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data.playlists.items;
+    }
+
+    const getTracks = async (token, tracksEndPoint) => {
+
+        const limit = 10;
+
+        const result = await fetch(`${tracksEndPoint}?limit=${limit}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data.items;
+    }
+
+    const getTrack = async (token, trackEndPoint) => {
+
+        const result = await fetch(`${trackEndPoint}`, {
+            method: 'GET',
+            headers: { 'Authorization' : 'Bearer ' + token}
+        });
+
+        const data = await result.json();
+        return data;
+    }
+
+    return {
+        getToken() {
+            return getToken();
+        },
+        getGenres(token) {
+            return getGenres(token);
+        },
+        getPlaylistByGenre(token, genreId) {
+            return getPlaylistByGenre(token, genreId);
+        },
+        getTracks(token, tracksEndPoint) {
+            return getTracks(token, tracksEndPoint);
+        },
+        getTrack(token, trackEndPoint) {
+            return getTrack(token, trackEndPoint);
+        }
+    }
+})();
