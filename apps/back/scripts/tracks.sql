919c41039c4895103a7504053cec8c3b3817c84d
Quentin Guiheneuc <quentin.guiheneuc@ynov.com>
2022-03-28 11:52:30 +0200

    add musique

--- apps/back/scripts/tracks.sql
+++ apps/back/scripts/tracks.sql
@@ -1,19 +1,23 @@
-INSERT INTO "track" VALUES (1, 'Smells Like a Teen Spirit', 'Nirvana', 'https://www.youtube.com/watch?v=hTWKbfoikeg' );
+INSERT INTO `track` VALUES (1, 'Smells Like a Teen Spirit', 'Nirvana', 'https://www.youtube.com/watch?v=hTWKbfoikeg' );
 
-INSERT INTO "track" VALUES (2, 'Like a Rolling Stone', 'Bob Dylan', 'https://www.youtube.com/watch?v=IwOfCgkyEj0' );
+INSERT INTO `track` VALUES (2, 'Like a Rolling Stone', 'Bob Dylan', 'https://www.youtube.com/watch?v=IwOfCgkyEj0' );
 
-INSERT INTO "track" VALUES (3, 'Imagine', 'John Lennon', 'https://www.youtube.com/watch?v=YkgkThdzX-8' );
+INSERT INTO `track` VALUES (3, 'Imagine', 'John Lennon', 'https://www.youtube.com/watch?v=YkgkThdzX-8' );
 
-INSERT INTO "track" VALUES (4, 'One', 'U2', 'https://www.youtube.com/watch?v=ftjEcrrf7r0' );
+INSERT INTO `track` VALUES (4, 'One', 'U2', 'https://www.youtube.com/watch?v=ftjEcrrf7r0' );
 
-INSERT INTO "track" VALUES (5, 'Billie Jean', 'Michael Jackson', 'https://www.youtube.com/watch?v=Zi_XLOBDo_Y' );
+INSERT INTO `track` VALUES (5, 'Billie Jean', 'Michael Jackson', 'https://www.youtube.com/watch?v=Zi_XLOBDo_Y' );
 
-INSERT INTO "track" VALUES (6, 'Hey Jude', 'Beatles', 'https://www.youtube.com/watch?v=A_MjCqQoLLA' );
+INSERT INTO `track` VALUES (6, 'Hey Jude', 'Beatles', 'https://www.youtube.com/watch?v=A_MjCqQoLLA' );
 
-INSERT INTO "track" VALUES (7, 'Satisfaction', 'Rolling stones', 'https://www.youtube.com/watch?v=9g7RmKF-gRI' );
+INSERT INTO `track` VALUES (7, 'Satisfaction', 'Rolling stones', 'https://www.youtube.com/watch?v=9g7RmKF-gRI' );
 
-INSERT INTO "track" VALUES (8, 'Bohemian Rhapsody', 'Queen', 'https://www.youtube.com/watch?v=fJ9rUzIMcZQ' );
+INSERT INTO `track` VALUES (8, 'Bohemian Rhapsody', 'Queen', 'https://www.youtube.com/watch?v=fJ9rUzIMcZQ' );
 
-INSERT INTO "track" VALUES (9, 'London Calling', 'The Clash', 'https://www.youtube.com/watch?v=XN7iEFVLf5c' );
+INSERT INTO `track` VALUES (9, 'London Calling', 'The Clash', 'https://www.youtube.com/watch?v=XN7iEFVLf5c' );
 
-INSERT INTO "track" VALUES (10, 'Veridis Quo', 'Daft Punk', 'https://www.youtube.com/watch?v=HhZaHf8RP6g&t=11s' );
+INSERT INTO `track` VALUES (10, 'Veridis Quo', 'Daft Punk', 'https://www.youtube.com/watch?v=HhZaHf8RP6g&t=11s' );
+
+INSERT INTO `track` VALUES (10, 'Stayin Alive', 'Bee Gees', 'https://www.youtube.com/watch?v=fNFzfwLM72c&ab_channel=BeeGeesVEVO' );
+
+INSERT INTO `track` VALUES (10, 'YMCA', 'Village People', 'https://www.youtube.com/watch?v=CS9OO0S5w2k&ab_channel=VillagePeople' );
