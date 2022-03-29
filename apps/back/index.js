10d977d98508d5669030351b990f6665d3ab7b7e
Corentin Faget <faget.corentin@gmail.com>
2022-03-29 09:09:11 +0200

    add player with play/pause logic and raw data

--- apps/back/index.js
+++ apps/back/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5820;
 
 const router = require('./app/router');
 
