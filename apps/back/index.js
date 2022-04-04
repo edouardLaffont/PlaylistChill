eb0f61d33f56f2e77ab74beb175ca714e7d7b4df
Corentin Faget <faget.corentin@gmail.com>
2022-04-05 00:08:39 +0200

    add CORS header

--- apps/back/index.js
+++ apps/back/index.js
@@ -8,6 +8,17 @@ const port = process.env.PORT || 5820;
 
 const router = require('./app/router');
 
+app.use(function (req, res, next) {
+   res.setHeader("Access-Control-Allow-Origin", "*");
+   res.setHeader("Access-Control-Allow-Credentials", "true");
+   res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+   res.setHeader(
+     "Access-Control-Allow-Headers",
+     "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers"
+   );
+   next();
+ });
+
 app.use('/v1', router);
 
 app.listen(port, _ => {
