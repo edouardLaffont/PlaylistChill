6ff4a55d8f53213f1b50f1680ec8bbdf7f9b36c2
Quentin Guiheneuc <quentin.guiheneuc@ynov.com>
2022-04-04 14:51:12 +0200

    previous and mofication next

--- apps/back/app/router.js
+++ apps/back/app/router.js
@@ -1,13 +1,13 @@
-const { Router } = require('express');
-const likeController = require('./controllers/likeController');
-const trackController = require('./controllers/trackController');
+const { Router } = require("express");
+const likeController = require("./controllers/likeController");
+const trackController = require("./controllers/trackController");
 
 const router = Router();
 
 // Track
-router.get('/tracks', trackController.getAllTracks);
-router.get('/tracks/:id', trackController.getOneTrack);
-router.get('/next', trackController.getNextTrack);
-
+router.get("/tracks", trackController.getAllTracks);
+router.get("/tracks/:id", trackController.getOneTrack);
+router.get("/next", trackController.getNextTrack);
+router.get("/prev", trackController.getPrevTrack);
 
 module.exports = router;
