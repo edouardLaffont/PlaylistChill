7fd18705f767a06a71f16e6290c01087a4f280f1
Corentin Faget <faget.corentin@gmail.com>
2022-05-03 14:39:19 +0200

    Logo redirect to Home page

--- apps/frontend/src/components/Layout/SideBar/index.tsx
+++ apps/frontend/src/components/Layout/SideBar/index.tsx
@@ -6,11 +6,14 @@ import library_icon from '../../../assets/icons/library_icon.png'
 import heart_icon from '../../../assets/icons/heart_icon.png'
 
 import SideBarButton from '../SideBarButton';
+import { Link } from 'react-router-dom';
 
 export default function SideBar() {
   return (
     <div className="h-full max-w-max bg-gradient-to-b from-blue-dark-lighter to-blue-dark">
-      <h1 className="font-logo text-4xl px-14 pt-5 text-white">MUSIC</h1>
+      <Link to='/'>
+        <h1 className="font-logo text-4xl px-14 pt-5 text-white">MUSIC</h1>
+      </Link>
       <ul className='space-y-8 ml-5 mt-14'>
         <SideBarButton icon={home_icon} text='Home' to='/' />
         <SideBarButton icon={library_icon} text='Suggestions' to='/suggestions' />
