038b49cac62a32aae02ae0308fc8b35993e8a333
Corentin Faget <faget.corentin@gmail.com>
2022-05-03 14:37:15 +0200

    add search for music list

--- apps/frontend/src/components/MusicList/index.tsx
+++ apps/frontend/src/components/MusicList/index.tsx
@@ -1,3 +1,4 @@
+import { useAppSelector } from "../../store/store"
 import { Music } from "../../types/Music"
 import MusicListItem from "../MusicListItem"
 
@@ -6,9 +7,14 @@ type props = {
 }
 
 export default function MusicList({ musics }: props) {
-
-    const displayMusics = musics.map((music: Music) => { return <MusicListItem key={music.id} music={music} currentMusicList={musics}/> })
-
+    const { search } = useAppSelector(store => store.music)
+    const displayMusics =
+        musics?.filter((music: Music) => {
+            return music.artist.toLowerCase().includes(search.toLowerCase())
+                || music.title.toLowerCase().includes(search.toLowerCase())
+        })
+            .map((music: Music) => <MusicListItem key={music.id} music={music} currentMusicList={musics} />)
+            
     return (
         <table className="w-full text-white">
             <thead className=" border-b border-white-transparant-19">
