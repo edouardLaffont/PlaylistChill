84f154fa0c9cc2c103467399dfc408d8c70724f6
Corentin Faget <faget.corentin@gmail.com>
2022-05-03 12:47:57 +0200

    add favorites with back data

--- apps/frontend/src/pages/Favorites/index.tsx
+++ apps/frontend/src/pages/Favorites/index.tsx
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import MusicList from '../../components/MusicList';
-import { Music } from '../../types/Music';
+
+import { useAppSelector } from '../../store/store';
 
 export default function Favorites() {
-    const  [musics, setMusics] = useState<Array<Music>>([])
+    const { user } = useAppSelector(store => store.auth)
 
     return (
         <div className="px-10 py-3">
             <h1 className='text-5xl text-white mb-5'>Your favorites songs</h1>
             <div>
-                <MusicList musics={musics} />
+                {user.tracks.length?
+                    <MusicList musics={user.tracks} /> :
+                    <p className='text-xl text-white'>You haven't liked any music yet</p>
+                }
+                
             </div>
         </div>
     );
